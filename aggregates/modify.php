7a88d2e3e1de5f5c886b34a01ab27adeeb29c78b
Brend Wanders <b.wanders@utwente.nl>
2012-06-06 15:54:39 +0000

    Adds modification aggregate.

---
+++ aggregates/modify.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Brend Wanders <b.wanders@utwente.nl>
+ */
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die('Meh.');
+
+/**
+ * The modification aggregator.
+ */
+class plugin_strata_aggregate_modify extends plugin_strata_aggregate {
+    function aggregate($values, $hint = null) {
+		$result = array();
+		foreach($values as $val) {
+			$result[] = str_replace('{}', $val, $hint);
+		}
+		return $result;
+    }
+
+    function getInfo() {
+        return array(
+            'desc'=>'Modifies every elements by replacing it with the hinted pattern.',
+			'hint'=>'a replacement pattern, use {} as placeholder for the old value',
+            'tags'=>array()
+        );
+    }
+}

cf9f232e0b7f892aa43838735cedee59ce07c553
Brend Wanders <b.wanders@utwente.nl>
2012-06-16 20:01:57 +0000

    Adds page template  from fwurg_wiki_template field

---
+++ action.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * DokuWiki Plugin fwurwiki (Action Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Brend Wanders <b.wanders@utwente.nl>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) die('Meh.');
+
+class action_plugin_fwurgwiki extends DokuWiki_Action_Plugin {
+    /**
+     * Register function called by DokuWiki to allow us
+     * to register events we're interested in.
+     *
+     * @param controller object the controller to register with
+     */
+    public function register(Doku_Event_Handler &$controller) {
+        $controller->register_hook('COMMON_PAGETPL_LOAD', 'BEFORE', $this, '_template_load');
+    }
+
+    public function _template_load(&$event, $param) {
+        if(!empty($_REQUEST['fwurg_wiki_template'])) {
+            // fetch template string from request
+            $template = $_REQUEST['fwurg_wiki_template'];
+
+            // fix windows-based browser newlines
+            $template = str_replace("\r\n", "\r", $template);
+
+            // set template parameters
+            $event->data['tpl'] = $template;
+            $event->data['doreplace'] = false;
+        }
+    }
+}
